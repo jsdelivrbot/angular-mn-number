802ab2b7629f0531acfcaa592eaaa33b12f71823
Darlan Mendonça <darlanmendonca@gmail.com>
2017-04-13 16:49:59 -0300

    try parse model and viewvalue, need to be finished

--- sources/angular/mn-passwor/mn-number.directive.js
+++ sources/angular/mn-passwor/mn-number.directive.js
@@ -9,18 +9,36 @@ function MnNumberDirective($compile, $parse) {
     require: 'ngModel',
   }
 
-  function link(scope, element, attributes) {
+  function link(scope, element, attributes, ngModel) {
     const dirtyInput = element[0].querySelector('input + input')
     if (dirtyInput) {
       element[0].removeChild(dirtyInput)
     }
+
     const input = element.find('input')
-    element[0].value = $parse(attributes.ngModel)(scope)
+    const isPercentage = element[0].getAttribute('percentage')
+    const value = isPercentage
+      ? $parse(attributes.ngModel)(scope) * 100
+      : $parse(attributes.ngModel)(scope)
+
+    if (angular.isDefined(value)) {
+      element[0].value = value
+    }
+
     input.attr('ng-model', attributes.ngModel)
     $compile(input)(scope)
 
     const event = document.createEvent('HTMLEvents')
     event.initEvent('change', false, true)
     element[0].querySelector('input').dispatchEvent(event)
+
+    input.on('change', () => {
+      const value = element[0].value
+      ngModel.$modelValue = value
+      // ngModel.$viewValue = value
+      // ngModel.$render()
+      console.log(ngModel.$modelValue)
+    })
+    // console.log(ngModel.$modelValue)
   }
 }
